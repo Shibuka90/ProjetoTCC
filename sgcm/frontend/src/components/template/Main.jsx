e4317405e5df03dace3985d741368b2054ee43e8
Shibuka90 <kshibukawa@gmail.com>
2021-09-11 12:56:20 -0300

    alterando

--- sgcm/frontend/src/components/template/Main.jsx
+++ sgcm/frontend/src/components/template/Main.jsx
@@ -6,7 +6,20 @@ import React from 'react'
 export default props =>
     <React.Fragment>
         <Header {...props} />
-        <main className="content">
-            Conteúdo
+        <main className="content container-fluid">
+            <div className="menu-main">
+                <a href="#/cadastro">
+                    <i className="fa fa-folder"></i><span>Cadastros Gerais</span>
+                </a>
+                <a href="#/agendamento">
+                    <i className="fa fa-calendar"></i><span>Agendamento</span>
+                </a>
+                <a href="#/atendimento">
+                    <i className="fa fa-users"></i><span>Atendimento</span>
+                </a>
+                <a href="#/prontuario">
+                    <i className="fa fa-first-aid"></i><span>Atendimento</span>
+                </a>
+            </div>
         </main>
     </React.Fragment>
--- sgcm/frontend/src/components/template/Nav.jsx
+++ sgcm/frontend/src/components/template/Nav.jsx
@@ -3,26 +3,26 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-       <nav className="menu">
+       <nav className="menu-nav">
            <a href="#/">
                <i className="fa fa-home"></i><span>Módulos</span>
            </a>
-           <a href="#/paciente">
+           <a href="#/cadastro/paciente">
                <i className="fa fa-user"></i><span>Pacientes</span>
            </a>
-           <a href="#/medico">
+           <a href="#/cadastro/medico">
                <i className="fa fa-user-md"></i><span>Médicos</span>
            </a>
-           <a href="#/convenio">
+           <a href="#/cadastro/convenio">
                <i className="fa fa-book"></i><span>Convênios</span>
            </a>
-           <a href="#/especialidades">
+           <a href="#/cadastro/especialidades">
                 <i class="fa fa-stethoscope"></i><span>Especialidades</span>
            </a>
-           <a href="#/serviços">
+           <a href="#/cadastro/serviços">
                 <i class="fa fa-medkit"></i><span>Serviços</span>
            </a>
-           <a href="#/usuarios">
+           <a href="#/cadastro/usuarios">
                <i className="fa fa-users"></i><span>Colaboradores</span>
            </a>
        </nav>
