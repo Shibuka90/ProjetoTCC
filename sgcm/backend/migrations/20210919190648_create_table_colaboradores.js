eec78be3ea186c72c30a8db17f49054bc228fddf
Shibuka90 <kshibukawa@gmail.com>
2021-09-19 17:21:30 -0300

    alterando

--- sgcm/backend/migrations/20210919190648_create_table_colaboradores.js
+++ sgcm/backend/migrations/20210919190648_create_table_colaboradores.js
@@ -1,8 +1,33 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('colaboradores', table => {
+        table.increments('codigo').primary()
+        table.string('nome', 50).notNull()
+        table.string('email', 50).notNull().unique()
+        table.string('cpf', 15).notNull().unique()
+        table.string('datanasc', 10).notNull()
+        table.string('estadocivil', 20).notNull()
+        table.string('sexo',2).notNull()
+        table.string('cargo', 50).notNull()
+        table.string('siglacr', 15)
+        table.string('cr', 15).unique()
+        table.string('ufcr', 2)
+        table.string('cep',8).notNull().references('cep').intoTable('cep')
+        table.string('tipo',15).notNull()
+        table.string('endereco', 100).notNull()
+        table.string('numero',5).notNull()
+        table.string('bairro', 100).notNull()
+        table.string('ciddade', 50).notNull()
+        table.string('ufcidade',2).notNull()
+        table.string('telddd',4)
+        table.string('tel',8)
+        table.string('celddd',4).notNull()
+        table.string('cel', 9).notNull()
+        table.boolean('admin').notNull().defaultTo(false)
   
-};
-
-exports.down = function(knex) {
+    })
+  };
   
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTable('colaboradores')
+  };
