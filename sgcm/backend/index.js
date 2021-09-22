87fe364a3ff4363ed14cba5b338cabe125b3005e
Shibuka90 <kshibukawa@gmail.com>
2021-09-21 23:34:21 -0300

    incluindo

--- sgcm/backend/index.js
+++ sgcm/backend/index.js
@@ -6,11 +6,10 @@ app.db = db
 
 consign()
     .then('./config/middlewares.js')
-    .then('./api/validation.js')
     .then('./api')
     .then('./config/routes.js')
     .into(app)
 
 app.listen(4000, () => {
-    console.log('Backend execuntando...')
+    console.log('Backend executando...')
 })
