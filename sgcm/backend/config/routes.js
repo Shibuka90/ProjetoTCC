4b6184b373c3e4ad4bf957987ad887856bab67e9
Shibuka90 <kshibukawa@gmail.com>
2021-09-21 23:06:36 -0300

    alterando/excluindo

--- sgcm/backend/config/routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    app.route('/usuarios')
-        .post(app.api.usuarios.save)
-        .post(app.api.usuarios.get)
-
-    app.route('/usuarios/:codigo')
-        .put(app.api.usuarios.save)
-}
